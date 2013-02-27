dd4196413ec45b72c8ba3de34d70d9e4e4c2581d
Atif Aziz <code@raboof.com>
2013-02-27 10:15:15 +0100

    Adding symbols package

--- pack.cmd
+++ pack.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 chcp 1252 > nul
-pushd "%~dp0"
+cd "%~dp0"
 if not exist dist md dist
 if not %errorlevel%==0 exit /b %errorlevel%
-call build /v:m && for %%i in (pkg\*.nuspec) do .nuget\NuGet pack %%i -OutputDirectory dist
+call build /v:m && for %%i in (pkg\*.nuspec) do .nuget\NuGet pack %%i -OutputDirectory dist -Symbols

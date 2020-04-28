5892bda76210dd29c45059eb84965fdb5bb279d0
Atif Aziz <code@raboof.com>
2020-04-28 14:56:18 +0200

    Shorten license header in files

--- src/CommandEventArgs.cs
+++ src/CommandEventArgs.cs
@@ -1,7 +1,4 @@
-#region License, Terms and Author(s)
-//
-// NotWebMatrix
-// Copyright (c) 2012 Atif Aziz. All rights reserved.
+#region Copyright (c) 2012 Atif Aziz. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
